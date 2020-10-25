486b835474b32883f85dd7c670bc3fbce1914d86
favorskij <favorskij@mail.ru>
2020-10-25 20:53:41 +1100

    Commit

--- src/main/java/com/securitystart/security/SecurityConfig.java
+++ src/main/java/com/securitystart/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.securitystart.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
@@ -29,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/user").hasRole("USER")
                 .antMatchers("/login").anonymous()
                 .antMatchers("/").permitAll()
+                .mvcMatchers(HttpMethod.POST,"/TestPost").permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login")
